1285506d586bcc3a9174bef7a4623bf3c3c7d798
LuciusCornelius88 <volodymir.melnyk@gmail.com>
2023-12-24 20:46:10 +0200

    module 3 changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,18 @@
 import { Component } from 'react';
 import Header from './Header/Header';
+import Search from './Search/Search';
 // import Counter from './Counter/Counter';
-import Modal from './Modal/Modal';
-import ToDoList from './ToDoList/ToDoList';
-import FormLogin from './FormLogin/FormLogin';
+// import Modal from './Modal/Modal';
+// import ToDoList from './ToDoList/ToDoList';
+// import FormLogin from './FormLogin/FormLogin';
 
 import { nanoid } from 'nanoid';
+import ContentInfo from './ContentInfo/ContentInfo';
 
 class App extends Component {
   state = {
     isShowModal: false,
+    searchText: '',
   };
 
   showModal = () => {
@@ -28,20 +31,25 @@ class App extends Component {
     console.log('newUser :>> ', newUser);
   };
 
+  handleSearch = searchText => {
+    this.setState({
+      searchText,
+    });
+  };
+
   render() {
     return (
       <div className="container">
         <Header showModal={this.showModal} />
+        <Search handleSearch={this.handleSearch} />
+        <ContentInfo searchText={this.state.searchText} />
         {/* <Counter /> */}
-        <ToDoList />
-        {this.state.isShowModal && (
+        {/* <ToDoList /> */}
+        {/* {this.state.isShowModal && (
           <Modal closeModal={this.closeModal}>
-            <FormLogin
-              createUser={this.createUser}
-              closeModal={this.closeModal}
-            />
+            <FormLogin createUser={this.createUser} closeModal={this.closeModal} />
           </Modal>
-        )}
+        )} */}
       </div>
     );
   }
